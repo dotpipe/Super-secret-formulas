81b5fc2bcfbb14868cbb51fc6ebc4573d54ab684
Saad Ishafq <saadishfaq@aol.com>
2019-09-24 21:25:25 -0400

    display file length as hex

--- bester.cpp
+++ bester.cpp
@@ -324,7 +324,7 @@ int main(int argc, char *argv[])
         int y = 0;
         // Segment size
         int bytes = 48000;
-        if (bytes < 48000)
+        if (file_len < bytes)
             bytes = file_len;
         // Segments are made to make reading the file in
         // much easier, and faster. We're only concentrating
@@ -345,8 +345,10 @@ int main(int argc, char *argv[])
             // Insert zip mark and file size
             vect_tbl_of_file = compress(vect_tbl_of_file);
             vect_tbl_of_file.insert(vect_tbl_of_file.begin(), "]");
-            vect_tbl_of_file.insert(vect_tbl_of_file.begin(), std::to_string(std::stoul(len_tmp, nullptr, 16)));
-            vect_tbl_of_file.insert(vect_tbl_of_file.begin(), "[");
+            std::stringstream ss;
+            ss << std::hex << std::stoul(len_tmp);
+            vect_tbl_of_file.insert(vect_tbl_of_file.begin(), ss.str());
+            vect_tbl_of_file.insert(vect_tbl_of_file.begin(), "[0x");
             vect_tbl_of_file.insert(vect_tbl_of_file.begin(), "RXIVE");
             // draw segment line
             vect_tbl_of_file.push_back("XIV#");
